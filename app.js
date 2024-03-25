c33210af3102d519f66bba0b52001aeb55818d6c
Farhan Ali <159815493+farhan0098@users.noreply.github.com>
2024-03-25 07:56:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// var studentName=["hamzi1 ", "ali", "hamza"]
+// var resStudentName=studentName.slice(1,2)
+// console.log(studentName,resStudentName)
+// var studentName = ["haziq ", "ali" ,"hamza"];
+// var studentScore= (200,300,400);
+// var index=0;
+// var studentTotal=(studentName[index],studentScore[index],studentTotal[index])
+// console.log(studentName[index],studentScore,studentTotal ,200/300*100,"%")
+// index++;
+// console.log(studentName[index],studentScore,studentTotal ,200/300*100,"%")
+// index++;
+// console.log(studentName[index],studentScore,studentTotal ,200/300*100,"%")
+// index++;
+var studentName = ["Babar", "Zain", "Hamza"];
+var studentScores = [400, 380, 450];
+var index = 0;
+// var percentage = (studentScores [index] / 500) *100;
+console.log("Scores of" + studentName [index] + "is" + studentScores [index] +" ."+  (studentScores [index] / 500) *100 + "%");
+index++
+console.log("Scores of" + studentName [index] + "is" + studentScores [index] +" ."+  (studentScores [index] / 500) *100 + "%");
+index++
+console.log("Scores of" + studentName [index] + "is" + studentScores [index] +" ."+ (studentScores [index] / 500) *100 + "%");
